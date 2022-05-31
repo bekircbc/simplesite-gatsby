fb77d125d1e8c6e8d4d3ddc915e7b926fc2406aa
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-31 14:11:30 +0200

    page-2.js als info.js changed and infopage features added..

---
+++ src/pages/info.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const InfoPage = () => (
+  <Layout>
+    <Seo title="Page two" />
+    <h1>Info Page</h1>
+    <p>Welcome to the info page</p>
+  </Layout>
+)
+
+export default InfoPage
