905ca2d25dc387e72e76ae4d88b24a2666a9dacd
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-01 14:43:19 +0200

    gatsby project with 3 page finished. bookspage added with api..

--- src/pages/books.js
+++ src/pages/books.js
@@ -31,25 +31,26 @@ const BooksPage = () => {
       <Seo title="Page three" />
       <h1>Books</h1>
       <p>Welcome to the books page</p>
-
-      {books.map((book, index) => {
-        return (
-          <div className="book" key={index}>
-            <img
-              src={`https://edwardtanguay.netlify.app/share/images/books/${book.idcode}.png`}
-              alt="book cover"
-            />
-            <div className="info">
-              <div className="title">{book.title}</div>
-              <div className="description">{book.description}</div>
-              <div className="buttonArea">
-                <button onClick={() => handleDelete(book)}>Delete</button>
-                <button onClick={() => handleEdit(book)}>Edit</button>
+      <div className="booksPage">
+        {books.map((book, index) => {
+          return (
+            <div className="book" key={index}>
+              <img
+                src={`https://edwardtanguay.netlify.app/share/images/books/${book.idcode}.png`}
+                alt="book cover"
+              />
+              <div className="info">
+                <div className="title">{book.title}</div>
+                <div className="description">{book.description}</div>
+                <div className="buttonArea">
+                  <button onClick={() => handleDelete(book)}>Delete</button>
+                  <button onClick={() => handleEdit(book)}>Edit</button>
+                </div>
               </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
     </Layout>
   )
 }
