2c68018795e3c07f0a3f9d4f9dca2436c97d4365
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-01 11:58:32 +0200

    still not working. there is .cache problem..

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,35 +1,37 @@
+import * as React from "react"
+
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-import React, { useState, useEffect } from "react"
-import axios from "axios"
+// import { useState, useEffect } from "react"
+// import axios from "axios"
 
-const [books, setBooks] = useState([])
-const bookUrl = "https://edwardtanguay.netlify.app/share/books.json"
+// const [books, setBooks] = useState([])
+// const bookUrl = "https://edwardtanguay.netlify.app/share/books.json"
 
-useEffect(() => {
-  ;(async () => {
-    const response = (await axios.get(bookUrl)).data
-    setBooks(response)
-  })()
-}, [])
+// useEffect(() => {
+//   ;(async () => {
+//     const response = (await axios.get(bookUrl)).data
+//     setBooks(response)
+//   })()
+// }, [])
 
-const handleDelete = book => {
-  const _books = books.filter(m => m.id !== book.id)
-  setBooks(_books)
-}
+// const handleDelete = book => {
+//   const _books = books.filter(m => m.id !== book.id)
+//   setBooks(_books)
+// }
 
-const handleEdit = book => {
-  book.title = book.title + " - FINISHED"
-  setBooks([...books])
-}
+// const handleEdit = book => {
+//   book.title = book.title + " - FINISHED"
+//   setBooks([...books])
+// }
 
 const BooksPage = () => (
   <Layout>
     <Seo title="Page three" />
     <h1>Books</h1>
     <p>Welcome to the books page</p>
-    {books.map((book, index) => {
+    {/* {books.map((book, index) => {
       return (
         <div className="book" key={index}>
           <img
@@ -38,15 +40,13 @@ const BooksPage = () => (
           />
           <div className="info">
             <div className="title">{book.title}</div>
-            <div className="description">{book.description}</div>
-            <div className="buttonArea">
+            <div className="description">{book.description}</div> 
+     <div className="buttonArea">
               <button onClick={() => handleDelete(book)}>Delete</button>
               <button onClick={() => handleEdit(book)}>Edit</button>
-            </div>
-          </div>
-        </div>
-      )
-    })}
+            </div> 
+    </div>
+        </div> */}
   </Layout>
 )
 
