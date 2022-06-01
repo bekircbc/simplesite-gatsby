511c8af52558e5ab5a90e1300cd185ca59fa3bd8
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-01 13:56:15 +0200

    books added to books page.. now is working..

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,53 +1,57 @@
 import * as React from "react"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-// import { useState, useEffect } from "react"
-// import axios from "axios"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
-// const [books, setBooks] = useState([])
-// const bookUrl = "https://edwardtanguay.netlify.app/share/books.json"
+const bookUrl = "https://edwardtanguay.netlify.app/share/books.json"
 
-// useEffect(() => {
-//   ;(async () => {
-//     const response = (await axios.get(bookUrl)).data
-//     setBooks(response)
-//   })()
-// }, [])
+const BooksPage = () => {
+  const [books, setBooks] = useState([])
 
-// const handleDelete = book => {
-//   const _books = books.filter(m => m.id !== book.id)
-//   setBooks(_books)
-// }
+  useEffect(() => {
+    ;(async () => {
+      const response = (await axios.get(bookUrl)).data
+      setBooks(response)
+    })()
+  }, [])
 
-// const handleEdit = book => {
-//   book.title = book.title + " - FINISHED"
-//   setBooks([...books])
-// }
+  const handleDelete = book => {
+    const _books = books.filter(m => m.id !== book.id)
+    setBooks(_books)
+  }
 
-const BooksPage = () => (
-  <Layout>
-    <Seo title="Page three" />
-    <h1>Books</h1>
-    <p>Welcome to the books page</p>
-    {/* {books.map((book, index) => {
-      return (
-        <div className="book" key={index}>
-          <img
-            src={`https://edwardtanguay.netlify.app/share/images/books/${book.idcode}.png`}
-            alt="book cover"
-          />
-          <div className="info">
-            <div className="title">{book.title}</div>
-            <div className="description">{book.description}</div> 
-     <div className="buttonArea">
-              <button onClick={() => handleDelete(book)}>Delete</button>
-              <button onClick={() => handleEdit(book)}>Edit</button>
-            </div> 
-    </div>
-        </div> */}
-  </Layout>
-)
+  const handleEdit = book => {
+    book.title = book.title + " - FINISHED"
+    setBooks([...books])
+  }
+  return (
+    <Layout>
+      <Seo title="Page three" />
+      <h1>Books</h1>
+      <p>Welcome to the books page</p>
+
+      {books.map((book, index) => {
+        return (
+          <div className="book" key={index}>
+            <img
+              src={`https://edwardtanguay.netlify.app/share/images/books/${book.idcode}.png`}
+              alt="book cover"
+            />
+            <div className="info">
+              <div className="title">{book.title}</div>
+              <div className="description">{book.description}</div>
+              <div className="buttonArea">
+                <button onClick={() => handleDelete(book)}>Delete</button>
+                <button onClick={() => handleEdit(book)}>Edit</button>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </Layout>
+  )
+}
 
 export default BooksPage
